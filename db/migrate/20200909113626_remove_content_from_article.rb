4d83de03f3e52e4e7099dd7b7bd94a28ce661ccd
KEI <>
2020-09-10 19:41:45 +0900

    フォロー機能とタイムライン実装

--- db/migrate/20200909113626_remove_content_from_article.rb
+++ db/migrate/20200909113626_remove_content_from_article.rb
@@ -2,4 +2,12 @@ class RemoveContentFromArticle < ActiveRecord::Migration[6.0]
   def change
     remove_column :articles, :content, :text
   end
+
+  def up
+    remove_column :articles, :content
+  end
+
+  def down
+    add_column :articles, :content, :text
+  end
 end
